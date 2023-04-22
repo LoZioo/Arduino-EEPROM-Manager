3c8ca004b6de474195967795325f92e068dc1cfe
LoZioo <60042676+LoZioo@users.noreply.github.com>
2023-04-22 15:20:56 +0200

    feat: updated help.

--- src/tool/proc.py
+++ src/tool/proc.py
@@ -3,20 +3,19 @@ import sys
 from packet import *
 
 def print_help() -> None:
-	print("Usage:\n  %s port task [task parameters] [file]\n" % sys.argv[0])
+	print("Usage:\n  %s port task [task parameters]\n" % sys.argv[0])
 
 	print("port:\t\t\t\t Select COM port.")
-	print("file:\t\t\t\t binary file to read from or write to.")
 	print()
 
 	print("Tasks:")
-	print("  help, --help, -h\t\t Show help.")
-	print("  test\t\t\t\t Connection test.")
-	print("  read base-address offset\t Read \"offset\" bytes starting from the \"base-address\" address and put the read content into the binary file.")
-	print("  write base-address\t\t Write the binary file starting from the \"base-address\" address.")
-	print("  dump\t\t\t\t Dump EEPROM content in the binary file.")
-	print("  clear\t\t\t\t Fill the entire EEPROM with zeros.")
-	print("  size\t\t\t\t Get the EEPROM size in bytes.")
+	print("  help, --help, -h\t\t\t Show help.")
+	print("  test\t\t\t\t\t Connection test.")
+	print("  read base-address offset file.bin\t Read \"offset\" bytes starting from the \"base-address\" address of the EEPROM and put the read content into the binary file \"file.bin\".")
+	print("  write base-address file.bin\t\t Write the binary file \"file.bin\" starting from the \"base-address\" address of the EEPROM.")
+	print("  dump file.bin\t\t\t\t Dump EEPROM content in the binary file \"file.bin\".")
+	print("  clear\t\t\t\t\t Fill the entire EEPROM with zeros.")
+	print("  size\t\t\t\t\t Get the EEPROM size in bytes.")
 
 def check_ack(packet: bytes, message: str = "ACK received.") -> bool:
 	ack = struct.unpack(PACKET_T, packet)
